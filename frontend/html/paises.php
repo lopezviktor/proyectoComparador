722a9baa6db30b57c16045ab48c0eca3322fa59f
Juan Manuel <jmconsentino@gmail.com>
2024-05-16 13:18:50 +0200

    Añadir include php

---
+++ frontend/html/paises.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/script.js"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Comparador de paises</title>
+    
+</head>
+<body>
+    <div id="contenedor">
+        <?php
+            include "../includes/nav.php"  //incluye el cóigo del archivo nav.php
+
+        ?>
+
+        <div id="contenedorPagina">
+            <div id="contenedorPaises">
+                <div id="contenedorPais">
+                    <img src="..." alt="...">
+                    <div class="infoPais"></div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+            include "../includes/footer.php"  //incluye el código del archivo footer.php
+
+        ?>
+    </div>
+</body>
+</html>
---
+++ frontend/includes/nav.php
@@ -0,0 +1,17 @@
+<nav id="nav">
+    <div class="logo">
+        <a href="index.php"><img src="../../imagenes/logo.png" alt="Logo comparador"></a>
+    </div>
+    <div class="buscadorNav">
+        <div class="iconoBuscar"></div>
+        <span class="espacioBuscador"></span>
+        <input type="text">
+    </div>
+        <ul class="listaNav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="paises.php">Países</a></li>
+            <li><a href="comparadorPaises.php">Comparador</a></li>
+            <li><a href="sobreNosotros.php">Sobre nosotros</a></li>
+            <li><a href="login.html">Login</a></li>
+        </ul>
+</nav>
