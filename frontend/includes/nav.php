7f53479a6a6feeff297edd1d872d94325543a194
Victor <victorlopezlopez93@gmail.com>
2024-05-18 17:51:40 +0200

    Login y LogOut en el NAV
    Cuando inicias sesión se queda la sesión guardada y aparece mensaje de bienvenida en el NAV

--- frontend/includes/nav.php
+++ frontend/includes/nav.php
@@ -1,22 +1,25 @@
+<?php
+session_start(); // Asegúrate de llamar a session_start() si no se ha llamado antes.
+?>
 <nav id="nav">
-
-    <nav id="nav">
-        <div class="logo">
-            <a href="index.php"><img src="../../imagenes/logo.png" alt="Logo comparador"></a>
-        </div>
-        <div class="buscadorNav">
-            <div class="iconoBuscar"></div>
-            <span class="espacioBuscador"></span>
-            <input type="text" id="inputBuscador" placeholder="Buscar país...">
-        </div>
-        <ul class="listaNav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="paises.php">Países</a></li>
-            <li><a href="comparadorPaises.php">Comparador</a></li>
-            <li><a href="sobreNosotros.php">Sobre nosotros</a></li>
-            <li><a href="../html/login.php">Acceder o Registrar</a></li>
-            <li><a href="../html/registroPaises.php">Registrar países</a></li>
-        </ul>
-    </nav>
-
+    <div class="logo">
+        <a href="index.php"><img src="../../imagenes/logo.png" alt="Logo comparador"></a>
+    </div>
+    <div class="buscadorNav">
+        <div class="iconoBuscar"></div>
+        <span class="espacioBuscador"></span>
+        <input type="text" id="inputBuscador" placeholder="Buscar país...">
+    </div>
+    <ul class="listaNav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="paises.php">Países</a></li>
+        <li><a href="comparadorPaises.php">Comparador</a></li>
+        <li><a href="sobreNosotros.php">Sobre nosotros</a></li>
+        <?php if (isset($_SESSION['usuario'])): ?>
+            <li class="nombre-usuario">Bienvenido, <?= htmlspecialchars($_SESSION['usuario']) ?></li>
+            <li><a href="../../backend/controlador/logout.php">Cerrar sesión</a></li>
+        <?php else: ?>
+            <li><a href="login.php">Acceder o Registrar</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
