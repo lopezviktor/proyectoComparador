cd476bd1cdd567fcacd7a033175a540deca894e0
Juan Manuel <jmconsentino@gmail.com>
2024-05-22 13:50:56 +0200

    Mostrar RegistrarPaises con usuario admin

--- frontend/includes/navSinBuscador.php
+++ frontend/includes/navSinBuscador.php
@@ -10,9 +10,20 @@ session_start(); // Asegúrate de llamar a session_start() si no se ha llamado a
         <li><a href="paises.php">Países</a></li>
         <li><a href="comparadorPaises.php">Comparador</a></li>
         <li><a href="sobreNosotros.php">Sobre nosotros</a></li>
+
+        <!-- Si el usuario es está logueado, mostrar Bienvenido y Cerrar Sesión -->
         <?php if (isset($_SESSION['usuario'])): ?>
             <li class="nombre-usuario">Bienvenido, <?= htmlspecialchars($_SESSION['usuario']) ?></li>
             <li><a href="../../backend/controlador/logout.php">Cerrar sesión</a></li>
+
+            <!-- Si el usuario es admin, mostrar Registro Países -->
+            <?php if ($_SESSION['usuario']=="admin"): ?> 
+                
+                <li><a href="../html/registroPaises.php">RegistroPaises</a></li>
+
+            <?php  endif; ?>
+            <!-- Aqui termina el if Registro Países-->
+
         <?php else: ?>
             <li><a href="login.php">Acceder o Registrar</a></li>
         <?php endif; ?>
