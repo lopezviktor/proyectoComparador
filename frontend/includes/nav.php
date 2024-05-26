568a4295bbae412f6908645efe6eec02d63fae9c
Victor <victorlopezlopez93@gmail.com>
2024-05-26 13:27:52 +0200

    Tabla modificar datos de Países para admin
    HTML, PHP, JS -> Para que el admin pueda modificar los datos de los países.

--- frontend/includes/nav.php
+++ frontend/includes/nav.php
@@ -16,17 +16,15 @@ session_start(); // Asegúrate de llamar a session_start() si no se ha llamado a
         <li><a href="comparadorPaises.php">Comparador</a></li>
         <li><a href="sobreNosotros.php">Sobre nosotros</a></li>
         <?php if (isset($_SESSION['usuario'])): ?>
-            <li class="nombre-usuario">Bienvenido, <?= htmlspecialchars($_SESSION['usuario']) ?></li>
-            <li><a href="../../backend/controlador/logout.php">Cerrar sesión</a></li>
+        <li class="nombre-usuario">
+            Bienvenido, <a href="../html/paginaPerfil.php"><?= htmlspecialchars($_SESSION['usuario']) ?></a>
+        </li>
+        <li><a href="../../backend/controlador/logout.php">Cerrar sesión</a></li>
 
-            <?php if ($_SESSION['usuario']=="admin"): ?> 
-                
-                <li><a href="../html/registroPaises.php">Registrar País</a></li>
-
-            <?php  endif; ?>
-                <!-- Aqui termina el if -->
-
-            
+        <?php if ($_SESSION['usuario'] == "admin"): ?> 
+            <li><a href="../html/registroPaises.php">Registrar País</a></li>
+            <li><a href="../html/tablaPaisesAdmin.php">Modificar País</a></li>
+        <?php endif; ?>
         <?php else: ?>
             <li><a href="login.php">Acceder o Registrar</a></li>
         <?php endif; ?>
