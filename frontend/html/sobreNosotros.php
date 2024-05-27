cc317406251502b5820ccce6874ab645def882e6
Juan Manuel <jmconsentino@gmail.com>
2024-05-27 08:52:56 +0200

    Añadir formato millares que se había perdido y algunos acentos en textos

--- frontend/html/sobreNosotros.php
+++ frontend/html/sobreNosotros.php
@@ -20,22 +20,30 @@
         <p>
         Para cualquier consulta, colaboración o sugerencia, no dudes en ponerte en contacto con nosotros. <br>
         Valoramos tus comentarios y nos esforzamos por mejorar continuamente. <br>
-        Puedes enviarnos un correo electrónico a comparadorPaises@comparatupais.com o llamarnos al +123 456 7890. <br>
-        También puedes seguirnos en nuestras redes sociales para estar al día con las últimas noticias y actualizaciones.
+        Puedes enviarnos un correo electrónico a comparadorPaises@comparatupais.com o llamarnos al +34 918 456 789. <br>
+        También puedes seguirnos en nuestras redes sociales para estar al día con las últimas noticias y 
+        actualizaciones.
 
         </p>
-        <h3>Terminos de servicio.</h3>
+        <h3>Términos de servicio.</h3>
         <p>
-        Al acceder y utilizar nuestro comparador de países, aceptas estar sujeto a los siguientes términos y condiciones. <br>
-        Este servicio se proporciona "tal cual", sin garantías de ningún tipo sobre la exactitud, adecuación o completitud de la información. 
-        No somos responsables de ningún error u omisión en la funcionalidad o contenido ni de las consecuencias que puedan derivarse de su uso. Nos reservamos el derecho de modificar estos términos en cualquier momento, por lo que te recomendamos revisarlos periódicamente.
+        Al acceder y utilizar nuestro comparador de países, aceptas estar sujeto a los siguientes términos y 
+        condiciones. <br>
+        Este servicio se proporciona "tal cual", sin garantías de ningún tipo sobre la exactitud, adecuación o 
+        completitud de la información. 
+        No somos responsables de ningún error u omisión en la funcionalidad o contenido ni de las consecuencias 
+        que puedan derivarse de su uso. Nos reservamos el derecho de modificar estos términos en cualquier 
+        momento, por lo que te recomendamos revisarlos periódicamente.
 
         </p>
-        <h3>Politica de privacidad</h3>
+        <h3>Política de privacidad</h3>
         <p>
         Nos comprometemos a proteger la privacidad de nuestros usuarios. <br>
-        Toda la información personal recopilada se utiliza exclusivamente para mejorar la experiencia del usuario y no se comparte con terceros sin el consentimiento explícito del usuario, salvo en lo estrictamente necesario para proporcionar el servicio solicitado. <br> 
-        Utilizamos medidas de seguridad estándar para proteger la información recopilada, aunque ningún método de transmisión por Internet o de almacenamiento electrónico es 100% seguro.
+        Toda la información personal recopilada se utiliza exclusivamente para mejorar la experiencia del 
+        usuario y no se comparte con terceros sin el consentimiento explícito del usuario, salvo en lo 
+        estrictamente necesario para proporcionar el servicio solicitado. <br> 
+        Utilizamos medidas de seguridad estándar para proteger la información recopilada, aunque ningún 
+        método de transmisión por Internet o de almacenamiento electrónico es 100% seguro.
 
         </p>
     </div>
