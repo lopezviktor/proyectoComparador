2cb19c41af9e9a011ebbe31db9edb97bd0ef9a3b
Victor <victorlopezlopez93@gmail.com>
2024-05-31 19:34:15 +0200

    Corrección errores en JS y mejor organización de scripts

--- frontend/html/registroUsuarios.php
+++ frontend/html/registroUsuarios.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../js/adminUsuarios.js" ></script>
     <link rel="stylesheet" href="../css/styles.css">
-    
     <title>Nuevo registro</title>
 </head>
 <body>
